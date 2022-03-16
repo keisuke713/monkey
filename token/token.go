0a350d93702f5e76eb2cc8bb0d87ace0097a024d
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-03-16 21:07:34 +0900

    define token type

---
+++ token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT" // add, foobar
+	INT   = "INT"   // 1,2,3
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUCNTION"
+	LET      = "LET"
+)

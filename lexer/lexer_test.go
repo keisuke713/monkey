35a9f3058ea15c0f02a8860a3a7d08ca48a63e91
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-03-25 21:09:17 +0900

    fix lexer_test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,24 +8,25 @@ import (
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
-	let ten = 10;
-	let add = fn(x, y) {
-		x + y;
-	}
+let ten = 10;
 
-	let result = add(five, ten);
-	!-/*5;
-	5 < 10 > 5;
+let add = fn(x, y) {
+  x + y;
+};
 
-	if (5 < 10) {
-		return true;
-	} else {
-		return false;
-	}
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 
-	10 == 10;
-	10 != 9;
-	`
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -56,6 +57,7 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "result"},
 		{token.ASSIGN, "="},
@@ -75,7 +77,7 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "5"},
 		{token.LT, "<"},
 		{token.INT, "10"},
-		{token.RT, ">"},
+		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.IF, "if"},
@@ -112,11 +114,13 @@ func TestNextToken(t *testing.T) {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }

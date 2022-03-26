59aa0f1ac54ac5314bf637595be6fed2025e06d0
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-03-26 11:37:44 +0900

    fix repl

--- main.go
+++ main.go
@@ -2,30 +2,32 @@ package main
 
 import (
 	"fmt"
-	_ "os"
-	_ "os/user"
+	"os"
+	"os/user"
 
-	"github.com/keisuke713/monkey/lexer"
-	"github.com/keisuke713/monkey/parser"
+	"github.com/keisuke713/monkey/repl"
+	_ "github.com/keisuke713/monkey/lexer"
+	_ "github.com/keisuke713/monkey/parser"
 	_ "github.com/keisuke713/monkey/repl"
 )
 
 func main() {
 	// for debug
-	input := "a + add(b * c) + d"
-	l := lexer.New(input)
-	p := parser.New(l)
-	if program := p.ParseProgram(); program != nil {
-		fmt.Print(program.Statements)
-	}
+	// input := "a + add(b * c) + d"
+	// l := lexer.New(input)
+	// p := parser.New(l)
+	// if program := p.ParseProgram(); program != nil {
+	// 	fmt.Print(program.Statements)
+	// }
 	// ex) b ./main.go:16
 
 	// original
-	// user, err := user.Current()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("Hello %s! this is the Monkey programming language!\n", user.Username)
-	// fmt.Println("Feel free to type in commands")
-	// repl.Start(os.Stdin, os.Stdout)
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! this is the Monkey programming language!\n", user.Username)
+	fmt.Println("Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
 }

1ec49cca5bd2cce14f62544676f048d7d6b2c979
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2022-03-29 21:58:38 +0900

    impl function

--- object/object.go
+++ object/object.go
@@ -1,7 +1,10 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/keisuke713/monkey/ast"
+	"strings"
 )
 
 const (
@@ -10,6 +13,7 @@ const (
 	NULL_OBJ    = "NULL"
 	RETURN_VALUE_OBJ  = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type ObjectType string
@@ -52,6 +56,12 @@ type Error struct {
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string { return "ERROR: " + e.Message }
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvionment()
+	env.outer = outer
+	return env
+}
+
 func NewEnvionment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
@@ -59,14 +69,43 @@ func NewEnvionment() *Environment {
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
